da60c88990861fbfa074f0e53f2cddedadf65aad
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2019-12-13 10:19:12 +0530

    Update sample_programscpp.cmake

--- sample_programscpp.cmake
+++ sample_programscpp.cmake
@@ -1,4 +1,5 @@
 cmake_minimum_required(VERSION 2.6)
-	add_executable(
-  beginend begin_end.cpp
-                 bool bool.cpp)
+	add_executable(constructor constructor.cpp
+	               digitsep digit_sep.cpp
+                       beginend begin_end.cpp
+                       bool bool.cpp)
