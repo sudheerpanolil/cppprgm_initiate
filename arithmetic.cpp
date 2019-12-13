908cd6fc88ccefea33dc960557b0daebc50f5168
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2019-12-13 10:20:41 +0530

    Create arithmetic.cpp

---
+++ arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  //*** Addition ***
+  int x = 25;
+  int y = 13;
+  double a = 255.75;
+  double b = 3;
+  cout << "Total of x + y = " << (x + y) << endl;
+  cout << "Subtraction of x - y = " << (x - y) << endl;
+  cout << "Multiplication of a * b = " << (a * b) << endl;
+  cout << "Division of a / b = " << (a / b) << endl;
+}
+© 2019 GitHub, Inc.
