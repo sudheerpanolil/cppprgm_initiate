642ef1660da047dfd914efd4e8e1946ad8fe5f32
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2019-12-13 10:23:52 +0530

    Update sample_programscpp.cmake

--- sample_programscpp.cmake
+++ sample_programscpp.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 2.6)
 	add_executable(constructor constructor.cpp
 	               arithmetic arithmetic.cpp
+		       abstract abstractCpp.cpp
 	               digitsep digit_sep.cpp
                        beginend begin_end.cpp
                        bool bool.cpp)
