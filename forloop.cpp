6d47dd7cac7868fc5b5417d947df09278eb986c2
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2019-12-18 10:20:07 +0530

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+   /* local variable Initialization */   int n = 1,times=5;
+
+   /* for loops execution */   for( n = 1; n <= times; n = n + 1 )
+   {
+      cout << "C++ for loops: " << n <<endl;
+   }
+   return 0;
+}
